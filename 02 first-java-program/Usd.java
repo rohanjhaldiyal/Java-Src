02b16f182ce7d2df8425d0953619df602a762c0d
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2022-02-13 12:32:34 +0530

    updated code

--- 02 first-java-program/Usd.java
+++ 02 first-java-program/Usd.java
@@ -7,7 +7,6 @@ public class Usd {
         double inr = in.nextDouble();
         double rate = 0.013;
         double usd = inr*rate;
-
         System.out.println("The amount in USD is: "+usd);
     }
 }
