650832d6b958b230e0071759baf4fa988c73c241
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2022-02-23 13:43:28 +0530

    updated code

--- 03 conditionals-loops/Sumall.java
+++ 03 conditionals-loops/Sumall.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Sumall {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int s = 0;
-        while(true) {
+        int sum = 0;
+        while (true) {
             int n = in.nextInt();
-            if(n == 0)
+            if (n == 0)
                 break;
             else
-                s = s+n;
+                sum = sum + n;
         }
-        System.out.println("The Sum is: "+s);
+        System.out.println("The Sum is: " + sum);
     }
 }
