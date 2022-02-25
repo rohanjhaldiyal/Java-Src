fb42f91e878fe4b41ade4e8a9b20159082871d39
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2022-02-25 15:07:31 +0530

    updated code

--- 03 conditionals-loops/ReverseString.java
+++ 03 conditionals-loops/ReverseString.java
@@ -4,7 +4,7 @@ public class ReverseString {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a String: ");
-        String s = in.next();
+        String s = in.nextLine();
         System.out.println("Reversed String is: " + reverse(s));
     }
 
