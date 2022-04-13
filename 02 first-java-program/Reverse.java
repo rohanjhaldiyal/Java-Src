1b861d309c779d8f97f70e3fe7f577441ba4da03
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2022-04-13 10:28:25 +0530

    updated code

--- 02 first-java-program/Reverse.java
+++ 02 first-java-program/Reverse.java
@@ -5,12 +5,13 @@ public class Reverse {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = in.nextInt();
-        int m = n; int rev = 0; 
-        while(m > 0) {
+        int m = n;
+        int rev = 0;
+        while (m > 0) {
             int rem = m % 10;
-            rev = rev*10 + rem;
+            rev = rev * 10 + rem;
             m = m / 10;
         }
-        System.out.println("The reverse is: "+rev);
+        System.out.println("The reverse is: " + rev);
     }
 }
